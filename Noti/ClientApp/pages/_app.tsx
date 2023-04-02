902b6b2947cdc0604adaf0625834ca3b91f00210
hikmet <aydin96tr@gmail.com>
2023-04-02 14:51:00 +0300

    test commit

--- Noti/ClientApp/pages/_app.tsx
+++ Noti/ClientApp/pages/_app.tsx
@@ -1,12 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Link from 'next/link'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-    <div>
-      <Link href={'/products'} >Products</Link>
-    </div>
     <Component {...pageProps} />
   </>
 }
