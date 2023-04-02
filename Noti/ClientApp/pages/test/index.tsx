cd0864bf8c3791be27dee4a1f48cd8fa60cce66a
Groophy Lifefor <groophylifefor@gmail.com>
2023-04-02 12:56:04 +0300

    First commit

---
+++ Noti/ClientApp/pages/test/index.tsx
@@ -0,0 +1,13 @@
+﻿import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
+const test: NextPage = () => {
+    return (
+        <h4>test</h4>
+    )
+}
+
+export default test
